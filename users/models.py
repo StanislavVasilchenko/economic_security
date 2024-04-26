f7db4deed25352825981cbe39980073f80eb99e0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-26 17:39:42 +0300

    Создание модели user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {
+    'blank': True,
+    'null': True
+}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(verbose_name='email address', unique=True)
+    phone_number = models.CharField(verbose_name='phone number', max_length=12, **NULLABLE)
+    verify_key = models.CharField(max_length=10, verbose_name='verification key', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['']
+
+    def __str__(self):
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
