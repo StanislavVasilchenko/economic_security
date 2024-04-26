19531919c9de2b549c3e5ab0851643d28f5a1daf
stanislav <stanislav.vasilchenko@yandex.ru>
2024-04-26 17:02:30 +0300

    Настройка статики

--- app_config/settings.py
+++ app_config/settings.py
@@ -111,10 +111,18 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
