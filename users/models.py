245e38141265f48b0cb4e4d5055d4887a09b2637
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-08 17:01:39 +0300

    Добавлен новый менеджер в модель пользователя

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from users.manager import UserManager
+
 NULLABLE = {
     'blank': True,
     'null': True
@@ -14,12 +16,13 @@ class User(AbstractUser):
     phone_number = models.CharField(verbose_name='phone number', max_length=12, **NULLABLE)
     verify_key = models.CharField(max_length=10, verbose_name='verification key', **NULLABLE)
 
+    objects = UserManager()
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['']
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return f'{self.email}'
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
