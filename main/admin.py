815db16b62fd2ac257a135f5c7196929921b5abd
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-08 17:03:24 +0300

    Регистрация моделей AgentAdmin, FileReport в админке

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Agent, FileReport
+
+
+@admin.register(Agent)
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'inn',
+                    'departure_date', 'date_of_inspection', 'counterparty_form',
+                    'report_status', 'is_active')
+
+
+@admin.register(FileReport)
+class FileReportAdmin(admin.ModelAdmin):
+    list_display = ('file_path', 'file_name', 'date_created',
+                    'agent')
