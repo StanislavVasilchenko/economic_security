31ba63bef035b421879cd1a1106b9a19d07b58f0
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-13 00:38:35 +0300

    Ключи для почты и редиса

--- private_keys.py
+++ private_keys.py
@@ -14,3 +14,11 @@ S_USER_EMAIL = os.environ.get('S_USER_EMAIL')
 S_USER_PASSWORD = os.environ.get('S_USER_PASSWORD')
 S_USER_FIRST_NAME = os.environ.get('S_USER_FIRST_NAME')
 S_USER_LAST_NAME = os.environ.get('S_USER_LAST_NAME')
+
+BROKER_URL = os.environ.get('BROKER_URL')
+RESULT_BACKEND = os.environ.get('RESULT_BACKEND')
+
+HOST_USER = os.getenv('EMAIL_HOST_USER')
+HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+HOST = os.getenv('EMAIL_HOST')
+PORT = os.getenv('EMAIL_PORT')
