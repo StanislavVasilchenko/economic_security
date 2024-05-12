def92302445baa96aad37dda8f674a702a73121d
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-13 00:39:01 +0300

    Функция для отправки почты

--- main/servisec.py
+++ main/servisec.py
@@ -1,13 +1,15 @@
-from datetime import datetime
+from django.core.mail import send_mail
 
 from main.models import Agent
+from app_config.settings import EMAIL_HOST_USER
 
 
-def send_email():
-    pass
+def send_email_to_agent(agent: Agent):
+    send_mail(
+        subject=f'Опросник КА {agent.name} ИНН {agent.inn}, договор размещения оборудования ООО "Сеть"',
+        from_email=EMAIL_HOST_USER,
+        message=f'Опросник + ссылка',
+        recipient_list=[agent.email],
+    )
 
 
-def get_agents_for_inspection():
-    time_now = datetime.now().strftime('%Y-%m-%d')
-    agents = Agent.objects.filter(date_of_inspection=time_now)
-    return agents
