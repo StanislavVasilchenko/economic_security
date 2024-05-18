b7bf7bd91168aa63d4ec6ba4609b9d7ca215a608
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-18 17:37:54 +0300

    Функция отправки писем вынисена в сервисы

--- main/tasks.py
+++ main/tasks.py
@@ -3,17 +3,18 @@ from datetime import datetime
 from celery import shared_task
 
 from main.models import Agent, ReportStatus
-from main.servisec import send_email_to_agent
+from main.servisec import send_email_to_agent, change_after_send_email
 
 
 @shared_task
 def get_agents_for_inspection():
+    """Задаса по выбору КА дата проверки которых совпадает с текущей датой и """
     time_now = datetime.now().strftime('%Y-%m-%d')
-    agents = Agent.objects.filter(date_of_inspection=time_now)
+    agents = Agent.objects.filter(date_of_inspection=time_now,
+                                  email__isnull=False,
+                                  report_status=ReportStatus.NOT_VERIFIED)
     if agents.count() > 0:
         for agent in agents:
             send_email_to_agent(agent)
 
-            agent.report_status = ReportStatus.UNDER_INSPECTION
-            agent.departure_date = time_now
-            agent.save()
+            change_after_send_email(agent)
