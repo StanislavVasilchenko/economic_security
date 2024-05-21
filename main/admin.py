3322d7c4b60ff76d16e96c1bfad486d31cdd343d
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-21 16:27:12 +0300

    Добавлены фильтр и поиск

--- main/admin.py
+++ main/admin.py
@@ -8,6 +8,8 @@ class AgentAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'inn',
                     'departure_date', 'date_of_inspection', 'counterparty_form',
                     'report_status', 'is_active')
+    search_fields = ('name', 'email', 'inn')
+    list_filter = ('report_status', 'counterparty_form',)
 
 
 @admin.register(FileReport)
