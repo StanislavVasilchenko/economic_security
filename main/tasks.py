d123ae3c1cd9ee477d94e956b89afb6cf5400b10
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-21 16:30:17 +0300

    + функция для выбора агентов у которых приблежается дата проверки

--- main/tasks.py
+++ main/tasks.py
@@ -1,6 +1,6 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
-from celery import shared_task
+from celery import shared_task, current_task
 
 from main.models import Agent, ReportStatus
 from main.servisec import send_email_to_agent, change_after_send_email
@@ -9,7 +9,7 @@ from main.servisec import send_email_to_agent, change_after_send_email
 @shared_task
 def get_agents_for_inspection():
     """Задаса по выбору КА дата проверки которых совпадает с текущей датой и """
-    time_now = datetime.now().strftime('%Y-%m-%d')
+    time_now = datetime.now() + timedelta(days=7)
     agents = Agent.objects.filter(date_of_inspection=time_now,
                                   email__isnull=False,
                                   report_status=ReportStatus.NOT_VERIFIED)
@@ -18,3 +18,13 @@ def get_agents_for_inspection():
             send_email_to_agent(agent)
 
             change_after_send_email(agent)
+
+
+@shared_task
+def check_date_of_inspection():
+    date_inspection = datetime.now().date() + timedelta(days=7)
+    agents = Agent.objects.filter(date_of_inspection=date_inspection)
+    if agents.count() > 0:
+        for agent in agents:
+            agent.report_status = ReportStatus.NOT_VERIFIED
+            agent.save()
