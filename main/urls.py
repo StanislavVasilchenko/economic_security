497b10c0e0fcba681cdb7f2f4ce1ac1f3a7a994f
stanislav <stanislav.vasilchenko@yandex.ru>
2024-05-21 16:31:05 +0300

    Добавлен контролер для вывода списка агентов для отправки писем и добавление агента

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 
 from main.apps import MainConfig
-from main.views import HomeView, AgentListView
+from main.views import HomeView, AgentListView, AgentForInspectionView, send_agent_report, AgentCreateView
 
 app_name = MainConfig.name
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
     path('agents/', AgentListView.as_view(), name='agents-list'),
+    path('inspection/', AgentForInspectionView.as_view(), name='agents-inspections'),
+    path('send/', send_agent_report, name='send'),
+    path('create/', AgentCreateView.as_view(), name='create-agent'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,13 @@
 from datetime import datetime, timedelta
 
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, CreateView
 
-from main.models import Agent
+from main.models import Agent, ReportStatus
 from main.servisec import context_data_index
+from main.tasks import get_agents_for_inspection
 
 
 class HomeView(TemplateView):
@@ -24,3 +27,30 @@ class AgentListView(ListView):
         context_data = super().get_context_data(**kwargs)
         context_data['object_list'] = Agent.objects.all().order_by('date_of_inspection')[:10]
         return context_data
+
+
+class AgentForInspectionView(TemplateView):
+    """Выбор КА для проверки"""
+    template_name = 'main/agents_for_inspections.html'
+    model = Agent
+
+    def get_context_data(self, **kwargs):
+        date_inspection = datetime.now().date() + timedelta(days=7)
+        context_data = super().get_context_data(**kwargs)
+        context_data['object_list'] = Agent.objects.filter(date_of_inspection=date_inspection,
+                                                           email__isnull=False,
+                                                           report_status=ReportStatus.NOT_VERIFIED)
+        return context_data
+
+
+class AgentCreateView(CreateView):
+    model = Agent
+    fields = '__all__'
+    success_url = reverse_lazy('main:agents-list')
+
+
+def send_agent_report(request):
+    """Ручная отправка писем """
+    if request.method == 'POST':
+        get_agents_for_inspection()
+        return render(request, 'main/agents_for_inspections.html')
